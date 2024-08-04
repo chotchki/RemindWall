cc57782c5c045aed51d9a3bc41b3d3e479557647
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2024-08-04 11:52:59 -0700

    I THINK I fixed the cloudatabase startup

--- RWPureSwift/Sources/DataModel/DataSchema.swift
+++ RWPureSwift/Sources/DataModel/DataSchema.swift
@@ -1,3 +1,4 @@
+import CoreData
 import Foundation
 import SwiftData
 
@@ -6,29 +7,28 @@ public struct DataSchema {
     public static let modelContainer: ModelContainer = {
         let schema = Schema([ReminderTimeModel.self, Settings.self, Trackee.self])
         
-        let mC : ModelContainer
-        if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
-            mC = try! ModelContainer(for: schema, migrationPlan: SettingsMigrationPlan.self, configurations: .init(isStoredInMemoryOnly: true))
+        let modelConfig = if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+            ModelConfiguration(schema: schema, isStoredInMemoryOnly: true, allowsSave: true, groupContainer: .automatic, cloudKitDatabase: .private("iCloud.com.remindwall.tracker"))
         } else {
             //Once I figure out production, I'll need to change this
-            mC = try! ModelContainer(for: schema, migrationPlan: SettingsMigrationPlan.self, configurations: .init(isStoredInMemoryOnly: false))
+            ModelConfiguration(schema: schema, isStoredInMemoryOnly: false, allowsSave: true, groupContainer: .automatic, cloudKitDatabase: .private("iCloud.com.remindwall.tracker"))
         }
         
-        
-        
+        let mC = try! ModelContainer(for: schema, migrationPlan: MigrationPlan.self, configurations: modelConfig)
+            
         //Ensure there is a Settings row always present
         var itemFetchDescriptor = FetchDescriptor<Settings>()
         itemFetchDescriptor.fetchLimit = 1
-        
+            
         guard try! mC.mainContext.fetch(itemFetchDescriptor).count == 0 else { return mC }
-        
+            
         mC.mainContext.insert(Settings())
-        
+            
         return mC
     }()
 }
 
-public enum SettingsMigrationPlan: SchemaMigrationPlan {
+public enum MigrationPlan: SchemaMigrationPlan {
     public static var schemas: [any VersionedSchema.Type] {
         [SchemaV1.self, SchemaV2.self]
     }
