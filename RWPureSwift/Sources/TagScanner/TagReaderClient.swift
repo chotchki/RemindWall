cad1df8dbdea4e85634ea3776c309da11d57b385
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2026-01-27 21:39:00 -0800

    Added stronger type checks

--- RWPureSwift/Sources/TagScanner/TagReaderClient.swift
+++ RWPureSwift/Sources/TagScanner/TagReaderClient.swift
@@ -11,23 +11,26 @@ import Deadline
 import Dependencies
 import DependenciesMacros
 import TagTypes
+import Tagged
 
 
 @DependencyClient
 public struct TagReaderClient: Sendable {
-    public var slotNames: @Sendable () -> [String] = { [] }
-    public var nextTagId: @Sendable (String, Duration) async -> ReaderState = {_,_ in .noTag}
+    public var slotNames: @Sendable () -> [SlotName] = { [] }
+    public var nextTagId: @Sendable (SlotName, Duration) async -> ReaderState = {_,_ in .noTag}
+    
+    public typealias SlotName = Tagged<TagReaderClient, String>
 }
 
 extension TagReaderClient: DependencyKey {
     public static var liveValue: Self {
         let slotManager = TKSmartCardSlotManager.default
         return Self(
-            slotNames: { slotManager?.slotNames ?? [] },
+            slotNames: { slotManager?.slotNames.map{ SlotName($0)} ?? [] },
             nextTagId: {
                 slotName, timeout in
                 
-                guard let slot = await slotManager?.getSlot(withName: slotName) else {
+                guard let slot = await slotManager?.getSlot(withName: slotName.rawValue) else {
                     return .readerError("couldn't get slot")
                 }
                 
